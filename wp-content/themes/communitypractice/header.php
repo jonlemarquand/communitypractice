a9a2b3d1fa522fbcbeb78a9d775745dbf6adb1fd
Jon Le Marquand <jon.lemarquand@gmail.com>
2020-07-29 21:49:36 +0000

    Initial homepage style

--- wp-content/themes/communitypractice/header.php
+++ wp-content/themes/communitypractice/header.php
@@ -3,10 +3,30 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&family=Oswald:wght@500&display=swap" rel="stylesheet">
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <div id="wrapper" class="hfeed">
+<header id="header">
+<?php if (is_user_logged_in()) { ?>
+
+    <div class="loginbar">
+    <a class="link-button log-button" href="<?php echo wp_logout_url(get_permalink()); ?>">
+        Log Out
+    </a>
+    </div>
+<?php } else { // not logged in ?>
+
+    <div class="loginbar">
+        <div class="link-button log-button">
+            Log In
+        </div>
+    </div>
+
+<?php } ?>
+</header>
+<?php /* ORIGINAL
 <header id="header">
 <div id="branding">
 <div id="site-title">
@@ -21,4 +41,5 @@
 <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
 </nav>
 </header>
+*/ ?>
 <div id="container">
--- wp-content/themes/communitypractice/index.php
+++ wp-content/themes/communitypractice/index.php
@@ -1,10 +1,45 @@
 <?php get_header(); ?>
-<main id="content">
+<main id="content" class="home-content">
+
+<?php /*
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
 <?php comments_template(); ?>
 <?php endwhile; endif; ?>
 <?php get_template_part( 'nav', 'below' ); ?>
 </main>
-<?php get_sidebar(); ?>
+<?php get_sidebar(); ?> */?>
+<?php if (is_user_logged_in()) { ?>
+
+<p>Welcome!</p>
+<?php } else { // not logged in ?>
+<div class="home-div">
+    <img class="home-logo" src="/wp-content/uploads/2020/07/2012-transparency-international-logo-subline-01.png"/>
+<h1>Community of Practice</h1>
+<div class="subtext">Bringing together Experts and Members 
+of the Open Contracting community.</div>
+</div>
+<div class="home-div">
+<p>Here you will find resources, discussions, events and contacts for those looking to implement or improve their practices.</p>
+<p>We encourage all visitors to register as a member, or if you have specific expertise in one or more category you can create an Expert profile and begin curating discussions and sharing your expertise.</p>
+</div>
+<div class="options-section">
+    <div class="next-step">
+        <div class="link-button">Register
+        </div>
+        <p>Join discussions, share your resources and get the most out of the Knowledge Base by signing up.</p>
+    </div>
+    <div class="next-step">
+        <div class="link-button">Explore
+        </div>
+        <p>Explore our Knowledge Base by category or region, see Expert profiles or find upcoming events.</p>
+    </div>
+    <div class="next-step">
+        <div class="link-button">Engage
+        </div>
+        <p>Join the discussion and see what other practitioners have to say.</p>
+    </div>
+</div>
+</main>
+<?php } ?>
 <?php get_footer(); ?>
